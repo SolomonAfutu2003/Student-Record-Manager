d29921f94db699c446205ce7f59e01b7ec5dfd90
SolomonTheCoder <solomonafutu1@gmail.com>
2025-06-24 22:33:43 +0100

    update

--- Frontend/SRM/src/lib/axios.js
+++ Frontend/SRM/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://student-record-manager-8e81.onrender.com/api";
+    : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
