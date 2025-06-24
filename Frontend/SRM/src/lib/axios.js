b14581e9d7c3dfb13b8900472d69e082ea619c3b
SolomonTheCoder <solomonafutu1@gmail.com>
2025-06-24 21:41:48 +0100

    update

--- Frontend/SRM/src/lib/axios.js
+++ Frontend/SRM/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //in production, there is no localhost, so we have to make it dynamic
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://student-record-manager-8e81.onrender.com/api"
 
 const api = axios.create({
   baseURL: BASE_URL,
