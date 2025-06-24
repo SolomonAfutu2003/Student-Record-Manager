75fb3905242cc8dd58f2a317fa4aafed90ed8391
SolomonTheCoder <solomonafutu1@gmail.com>
2025-06-24 23:41:24 +0100

    up

--- Frontend/SRM/src/lib/axios.js
+++ Frontend/SRM/src/lib/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 //in production, there is no localhost, so we have to make it dynamic
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const isLocalhost = window.location.hostname === "localhost";
 
-console.log(import.meta.env.MODE);
+const BASE_URL = isLocalhost
+  ? "http://localhost:3000/api"
+  : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
 export default api;
+
